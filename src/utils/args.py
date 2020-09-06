893ed50f111800f28095b875c3ce458d1afe9e5d
Antoine Maillard <antoine.maillard@epitech.eu>
2020-09-06 17:49:35 +0200

    add util to get next args for multiple use

--- src/utils/args.py
+++ src/utils/args.py
@@ -8,9 +8,23 @@ def is_arg(list_arg: [str], arg: str):
 
 def get_next_arg(list_arg: [str], arg: str):
     is_next_arg = False
+
     for actual_arg in list_arg:
         if is_next_arg:
             return actual_arg
         if actual_arg == arg:
             is_next_arg = True
     return ""
+
+def get_next_args(list_arg: [str], arg: str):
+    next_args = []
+    is_next_arg = False
+
+    for actual_arg in list_arg:
+        if is_next_arg and actual_arg[0] == '-':
+            return next_args
+        elif is_next_arg:
+            next_args.append(actual_arg)
+        if actual_arg == arg:
+            is_next_arg = True
+    return next_args
