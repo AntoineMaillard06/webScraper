6d91b9ac42c53b004fb826c328c9ff6962ed5af0
Antoine Maillard <antoine.maillard@epitech.eu>
2020-09-06 15:54:13 +0200

    end function ok and texts command ok

--- src/get_element.py
+++ src/get_element.py
@@ -11,27 +11,45 @@ def cmd_help():
     return 0
 
 def cmd_text(content):
-    return content.get_text()
+    return content.get_text() + "\n_____\n"
 
-def cmd_wordline(content, word):
+def cmd_text_wordline(content, word):
     text = content.get_text().split('\n')
-    response = word + " match(s):\n"
+    response = word + " word match(s):\n"
 
     for line in text:
         if word.lower() in line.lower():
             response += line + '\n'
+    response += "_____\n"
     return response
+
+def cmd_text_attribute(content, attribute):
+    text = content.find_all(attribute)
+    response = attribute + " attribute(s) texts:\n"
+
+    for line in text:
+        response += str(line.string) + '\n'
+    response += "_____\n"
+    return response
+
+def exec_response(response):
+    print(response["text"])
+    if response["file"] and len(response["filename"]) > 0:
+        f = open(response["filename"], "a")
+        f.write(response["text"])
+        f.close()
+    return 0
 
 def scrap_page(content, input_stream):
     content = BeautifulSoup(content, 'html.parser')
+    response = {
+        "text": "",
+        "file": False,
+        "filename": ""
+    }
 
     for line in input_stream:
         array_cmd = line.split()
-        response = {
-            "text": "",
-            "file": False,
-            "filename": ""
-        }
         if is_arg(array_cmd, "-f"):
             response["file"] = True
             response["filename"] = get_next_arg(array_cmd, "-f")
@@ -41,6 +59,8 @@ def scrap_page(content, input_stream):
             if len(array_cmd) == 1:
                 response["text"] += cmd_text(content)
             elif is_arg(array_cmd, "-w"):
-                response["text"] += cmd_wordline(content, get_next_arg(array_cmd, "-w"))
-        print(response["text"])
+                response["text"] += cmd_text_wordline(content, get_next_arg(array_cmd, "-w"))
+            elif is_arg(array_cmd, "-a"):
+                response["text"] += cmd_text_attribute(content, get_next_arg(array_cmd, "-a"))
+    exec_response(response)
     return 0
